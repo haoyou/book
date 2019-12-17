b8d547d3410c95b8d0d1825b42f2ee72b3448f12
chenhaoyou <1437177646@qq.com>
2019-12-18 00:56:41 +0800

    commit all java files

---
+++ bookmaster/src/main/java/com/chy/bookmaster/BookmasterApplication.java
@@ -0,0 +1,12 @@
+package com.chy.bookmaster;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookmasterApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(BookmasterApplication.class, args);
+    }
+
+}
---
+++ bookmaster/src/test/java/com/chy/bookmaster/BookmasterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chy.bookmaster;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookmasterApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
