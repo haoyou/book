80f67a393d435e7d0414c1b42a167b861215af84
chenhaoyou <1437177646@qq.com>
2019-12-19 23:57:21 +0800

    新增查询所有用户信息

--- gmall/gmall-user/src/main/java/com/gamll/chy/gmalluser/controller/UserController.java
+++ gmall/gmall-user/src/main/java/com/gamll/chy/gmalluser/controller/UserController.java
@@ -1,4 +1,30 @@
 package com.gamll.chy.gmalluser.controller;
 
+import com.gamll.chy.gmalluser.entity.UmsMember;
+import com.gamll.chy.gmalluser.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.List;
+
+@Controller
+@RequestMapping("/user")
 public class UserController {
+    @Autowired
+    UserService userService;
+
+    @RequestMapping("index")
+    @ResponseBody
+    public String index(){
+        return "index";
+    }
+
+    @RequestMapping("/getalluserinfo")
+    @ResponseBody
+    public List<UmsMember> getAllUserInfo(){
+        List<UmsMember> umsMembers = userService.getAllUserInfo();
+        return umsMembers;
+    }
 }
