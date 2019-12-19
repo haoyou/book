a2ceb27cb6798b341f8623d5a8b99941264d98cf
chenhaoyou <1437177646@qq.com>
2019-12-19 23:58:08 +0800

    新增查询所有用户信息

--- gmall/gmall-user/src/main/java/com/gamll/chy/gmalluser/GmallUserApplication.java
+++ gmall/gmall-user/src/main/java/com/gamll/chy/gmalluser/GmallUserApplication.java
@@ -1,9 +1,11 @@
 package com.gamll.chy.gmalluser;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@MapperScan(basePackages = "com.gamll.chy.gmalluser.mapper")
 public class GmallUserApplication {
 
     public static void main(String[] args) {
