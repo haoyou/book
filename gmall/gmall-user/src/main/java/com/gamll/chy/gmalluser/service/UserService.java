ffb0459a7816c930c2edf6235e477533cdbbf56f
chenhaoyou <1437177646@qq.com>
2019-12-19 23:57:53 +0800

    新增查询所有用户信息

--- gmall/gmall-user/src/main/java/com/gamll/chy/gmalluser/service/UserService.java
+++ gmall/gmall-user/src/main/java/com/gamll/chy/gmalluser/service/UserService.java
@@ -1,4 +1,9 @@
 package com.gamll.chy.gmalluser.service;
 
+import com.gamll.chy.gmalluser.entity.UmsMember;
+
+import java.util.List;
+
 public interface UserService {
+    public List<UmsMember> getAllUserInfo();
 }
