a6c869eb12be487f9614fbb9350687be74d4b8b3
chenhaoyou <1437177646@qq.com>
2019-12-20 00:06:07 +0800

    新增查询所有用户信息

---
+++ gmall/gmall-user/src/main/java/com/gamll/chy/gmalluser/entity/UmsMember.java
@@ -0,0 +1,202 @@
+package com.gamll.chy.gmalluser.entity;
+
+import java.util.Date;
+
+public class UmsMember {
+    private String id;
+    private String  memberLlevelId;
+    private String  username;
+    private String  password;
+    private String  nickname;
+    private String  phone;
+    private Integer  status;
+    private Date create_time;
+    private String  icon;
+    private Integer  gender;
+    private Date  birthday;
+    private String  city;
+    private String  job;
+    private String  personalizedSignature;
+    private Integer  sourceType;
+    private Integer  integration;
+    private Integer  growth;
+    private Integer  luckeyCount;
+    private Integer  historyIntegration;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getMemberLlevelId() {
+        return memberLlevelId;
+    }
+
+    public void setMemberLlevelId(String memberLlevelId) {
+        this.memberLlevelId = memberLlevelId;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getNickname() {
+        return nickname;
+    }
+
+    public void setNickname(String nickname) {
+        this.nickname = nickname;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public Integer getStatus() {
+        return status;
+    }
+
+    public void setStatus(Integer status) {
+        this.status = status;
+    }
+
+    public Date getCreate_time() {
+        return create_time;
+    }
+
+    public void setCreate_time(Date create_time) {
+        this.create_time = create_time;
+    }
+
+    public String getIcon() {
+        return icon;
+    }
+
+    public void setIcon(String icon) {
+        this.icon = icon;
+    }
+
+    public Integer getGender() {
+        return gender;
+    }
+
+    public void setGender(Integer gender) {
+        this.gender = gender;
+    }
+
+    public Date getBirthday() {
+        return birthday;
+    }
+
+    public void setBirthday(Date birthday) {
+        this.birthday = birthday;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getJob() {
+        return job;
+    }
+
+    public void setJob(String job) {
+        this.job = job;
+    }
+
+    public String getPersonalizedSignature() {
+        return personalizedSignature;
+    }
+
+    public void setPersonalizedSignature(String personalizedSignature) {
+        this.personalizedSignature = personalizedSignature;
+    }
+
+    public Integer getSourceType() {
+        return sourceType;
+    }
+
+    public void setSourceType(Integer sourceType) {
+        this.sourceType = sourceType;
+    }
+
+    public Integer getIntegration() {
+        return integration;
+    }
+
+    public void setIntegration(Integer integration) {
+        this.integration = integration;
+    }
+
+    public Integer getGrowth() {
+        return growth;
+    }
+
+    public void setGrowth(Integer growth) {
+        this.growth = growth;
+    }
+
+    public Integer getLuckeyCount() {
+        return luckeyCount;
+    }
+
+    public void setLuckeyCount(Integer luckeyCount) {
+        this.luckeyCount = luckeyCount;
+    }
+
+    public Integer getHistoryIntegration() {
+        return historyIntegration;
+    }
+
+    public void setHistoryIntegration(Integer historyIntegration) {
+        this.historyIntegration = historyIntegration;
+    }
+
+    @Override
+    public String toString() {
+        return "UmsMember{" +
+                "id='" + id + '\'' +
+                ", memberLlevelId='" + memberLlevelId + '\'' +
+                ", username='" + username + '\'' +
+                ", password='" + password + '\'' +
+                ", nickname='" + nickname + '\'' +
+                ", phone='" + phone + '\'' +
+                ", status=" + status +
+                ", create_time=" + create_time +
+                ", icon='" + icon + '\'' +
+                ", gender=" + gender +
+                ", birthday=" + birthday +
+                ", city='" + city + '\'' +
+                ", job='" + job + '\'' +
+                ", personalizedSignature='" + personalizedSignature + '\'' +
+                ", sourceType=" + sourceType +
+                ", integration=" + integration +
+                ", growth=" + growth +
+                ", luckeyCount=" + luckeyCount +
+                ", historyIntegration=" + historyIntegration +
+                '}';
+    }
+}
