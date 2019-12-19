30fd0741c669f4336394327cb18701697174d8b0
chenhaoyou <1437177646@qq.com>
2019-12-19 23:57:39 +0800

    新增查询所有用户信息

--- gmall/gmall-user/src/main/java/com/gamll/chy/gmalluser/service/impl/UserServiceImpl.java
+++ gmall/gmall-user/src/main/java/com/gamll/chy/gmalluser/service/impl/UserServiceImpl.java
@@ -1,6 +1,22 @@
 package com.gamll.chy.gmalluser.service.impl;
 
+import com.gamll.chy.gmalluser.entity.UmsMember;
+import com.gamll.chy.gmalluser.mapper.UserMapper;
 import com.gamll.chy.gmalluser.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
+import java.util.List;
+
+@Service
 public class UserServiceImpl implements UserService {
+    @Autowired
+    UserMapper userMapper;
+
+
+    @Override
+    public List<UmsMember> getAllUserInfo() {
+        List<UmsMember> umsMembers = userMapper.selectAllUserInfo();
+        return umsMembers;
+    }
 }
